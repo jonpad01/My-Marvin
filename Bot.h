5447213451be9f392e04df3adb80872ce7866e11
jonpad01 <62527756+jonpad01@users.noreply.github.com>
2020-07-12 21:18:16 -0700

    Add files via upload
    updates for extreme games and pulling attach code out of bot.cpp and into the behavior trees

---
+++ Bot.h
@@ -0,0 +1,126 @@
+#pragma once
+
+#include <fstream>
+#include <memory>
+
+#include "KeyController.h"
+#include "RayCaster.h"
+#include "RegionRegistry.h"
+#include "Steering.h"
+#include "behavior/BehaviorEngine.h"
+#include "path/Pathfinder.h"
+
+
+
+
+namespace marvin {
+
+    using Path = std::vector<Vector2f>;
+
+extern std::ofstream debug_log;
+struct Area {
+    bool in_base, in_tunnel, in_center;
+    bool in_1, in_2, in_3, in_4, in_5, in_6, in_7, in_8;
+    bool connected, in_diff;
+    std::vector<bool> in;
+};
+struct Flaggers {
+    Player enemy_anchor, team_anchor;
+    int flaggers, team_lancs, team_spiders;
+    
+};
+struct Duelers {
+    int freq_0s, freq_1s;
+    std::vector< Player > team;
+    bool team_in_base, enemy_in_base;
+};
+
+class GameProxy;
+struct Player;
+
+class Bot {
+public:
+    Bot(std::unique_ptr<GameProxy> game);
+    void Hyperspace();
+    void Devastation();
+    void ExtremeGames();
+
+  void Update(float dt);
+
+  KeyController& GetKeys() { return keys_; }
+  GameProxy& GetGame() { return *game_; }
+
+  void Move(const Vector2f& target, float target_distance);
+  path::Pathfinder& GetPathfinder() { return *pathfinder_; }
+
+  const RegionRegistry& GetRegions() const { return *regions_; }
+
+  SteeringBehavior& GetSteering() { return steering_; }
+
+  uint64_t GetTime() const;
+
+  void AddBehaviorNode(std::unique_ptr<behavior::BehaviorNode> node) {
+      behavior_nodes_.push_back(std::move(node));
+  }
+
+  void SetBehavior(behavior::BehaviorNode* behavior) {
+      behavior_ = std::make_unique<behavior::BehaviorEngine>(behavior);
+  }
+
+
+  Area InArea(Vector2f position);
+
+  int BaseSelector();
+
+  Flaggers FindFlaggers();
+  Duelers FindDuelers();
+  bool CheckStatus();
+  int FindOpenFreq();
+
+ private:
+  void Steer();
+
+  std::unique_ptr<GameProxy> game_;
+  std::unique_ptr<path::Pathfinder> pathfinder_;
+  std::unique_ptr<behavior::BehaviorEngine> behavior_;
+  std::unique_ptr<RegionRegistry> regions_;
+  behavior::ExecuteContext behavior_ctx_;
+  int ship_;
+  uint64_t last_ship_change_;
+  uint64_t last_base_change_;
+  SteeringBehavior steering_;
+
+  std::vector<std::unique_ptr<behavior::BehaviorNode>> behavior_nodes_;
+
+  // TODO: Action-key map would be more versatile
+  KeyController keys_;
+};
+
+bool WallShot(behavior::ExecuteContext& ctx, const marvin::Player& bot_player);
+
+bool InRect(marvin::Vector2f pos, marvin::Vector2f min_rect,
+    marvin::Vector2f max_rect);
+
+bool IsValidPosition(marvin::Vector2f position);
+
+bool CanShoot(const marvin::Map& map, const marvin::Player& bot_player,
+    const marvin::Player& target);
+
+marvin::Vector2f CalculateShot(const marvin::Vector2f& pShooter,
+    const marvin::Vector2f& pTarget,
+    const marvin::Vector2f& vShooter,
+    const marvin::Vector2f& vTarget,
+    float sProjectile);
+
+class PathingNode : public behavior::BehaviorNode {
+public:
+
+    virtual behavior::ExecuteResult Execute(behavior::ExecuteContext& ctx) override = 0;
+
+    float PathLength(behavior::ExecuteContext& ctx, Vector2f from, Vector2f to);
+        
+protected:
+    Path CreatePath(behavior::ExecuteContext& ctx, const std::string& pathname, Vector2f from, Vector2f to, float radius);
+};
+
+}  // namespace marvin
---
+++ main.cpp
@@ -0,0 +1,188 @@
+#include "platform/Platform.h"
+//
+#include <detours.h>
+
+#include <chrono>
+#include <fstream>
+#include <iostream>
+
+#include "Bot.h"
+#include "Debug.h"
+#include "Map.h"
+#include "path/Pathfinder.h"
+#include "platform/ContinuumGameProxy.h"
+#include "platform/ExeProcess.h"
+
+const char* kEnabledText = "Continuum (enabled)";
+const char* kDisabledText = "Continuum (disabled)";
+
+
+using time_clock = std::chrono::high_resolution_clock;
+using time_point = time_clock::time_point;
+using seconds = std::chrono::duration<float>;
+
+std::unique_ptr<marvin::Bot> g_Bot;
+static bool g_Enabled = true;
+//static HWND g_hWnd;
+HWND g_hWnd = 0;
+static time_point g_LastUpdateTime;
+
+static SHORT(WINAPI* RealGetAsyncKeyState)(int vKey) = GetAsyncKeyState;
+static BOOL(WINAPI* RealPeekMessageA)(LPMSG lpMsg, HWND hWnd,
+                                      UINT wMsgFilterMin, UINT wMsgFilterMax,
+                                      UINT wRemoveMsg) = PeekMessageA;
+
+SHORT WINAPI OverrideGetAsyncKeyState(int vKey) {
+#if DEBUG_USER_CONTROL
+    if (1) {
+#else
+  if (!g_Enabled) {
+#endif
+    //return RealGetAsyncKeyState(vKey);
+      if (GetActiveWindow() == g_hWnd) {
+          return RealGetAsyncKeyState(vKey);
+      }
+
+      return 0;
+  }
+
+  if (g_Bot->GetKeys().IsPressed(vKey)) {
+    return (SHORT)0x8000;
+  }
+
+  return 0;
+}
+
+// This is used to hook into the main update loop in Continuum so the bot can be
+// updated.
+BOOL WINAPI OverridePeekMessageA(LPMSG lpMsg, HWND hWnd, UINT wMsgFilterMin,
+                                 UINT wMsgFilterMax, UINT wRemoveMsg) {
+  // Check for key presses to enable/disable the bot.
+  if (GetActiveWindow() == g_hWnd) {
+    if (RealGetAsyncKeyState(VK_F10)) {
+      g_Enabled = false;
+      SetWindowText(g_hWnd, kDisabledText);
+    } else if (RealGetAsyncKeyState(VK_F9)) {
+      g_Enabled = true;
+      SetWindowText(g_hWnd, kEnabledText);
+    }
+  }
+
+  time_point now = time_clock::now();
+  seconds dt = now - g_LastUpdateTime;
+
+  if (g_Enabled) {
+    g_Bot->Update(dt.count());
+  }
+
+  g_LastUpdateTime = now;
+
+#if DEBUG_RENDER
+  marvin::WaitForSync();
+#endif
+
+
+  return RealPeekMessageA(lpMsg, hWnd, wMsgFilterMin, wMsgFilterMax,
+                          wRemoveMsg);
+}
+
+/*HWND GetMainWindow() {
+  DWORD proc_id = GetCurrentProcessId();
+  HWND hwnd = nullptr;
+
+  char title[1024];
+
+  do {
+    hwnd = FindWindowEx(nullptr, hwnd, nullptr, nullptr);
+    DWORD check_id;
+    GetWindowThreadProcessId(hwnd, &check_id);
+
+    if (check_id == proc_id) {
+      GetWindowText(hwnd, title, 1024);
+
+      if (strcmp(title, "Continuum") == 0) {
+      return hwnd;
+      }
+    }
+  } while (hwnd);
+
+  return NULL;
+}*/
+
+BOOL CALLBACK MyEnumWindowsProc(HWND hwnd, LPARAM lParam) {
+    DWORD pid;
+
+    GetWindowThreadProcessId(hwnd, &pid);
+
+    if (pid == lParam) {
+        g_hWnd = hwnd;
+        return FALSE;
+    }
+
+    return TRUE;
+}
+
+HWND GetMainWindow() {
+    DWORD pid = GetCurrentProcessId();
+
+    EnumWindows(MyEnumWindowsProc, pid);
+
+    return g_hWnd;
+}
+
+marvin::Bot& CreateBot() {
+  auto proxy = std::make_unique<marvin::ContinuumGameProxy>(g_hWnd);
+
+  g_Bot = std::make_unique<marvin::Bot>(std::move(proxy));
+
+  return *g_Bot;
+}
+
+extern "C" __declspec(dllexport) void InitializeMarvin() {
+  g_hWnd = GetMainWindow();
+
+  marvin::debug_log.open("marvin.log", std::ios::out | std::ios::app);
+
+  marvin::debug_log << "Starting Marvin.\n";
+  
+  try {
+    CreateBot();
+  } catch (std::exception& e) {
+    MessageBox(NULL, e.what(), "A", MB_OK);
+  }
+
+  DetourRestoreAfterWith();
+
+  DetourTransactionBegin();
+  DetourUpdateThread(GetCurrentThread());
+  DetourAttach(&(PVOID&)RealGetAsyncKeyState, OverrideGetAsyncKeyState);
+  DetourAttach(&(PVOID&)RealPeekMessageA, OverridePeekMessageA);
+  DetourTransactionCommit();
+
+  
+  char buf[1024];
+  GetWindowText(g_hWnd, buf, 1024);
+  if (strcmp(buf, "Continuum") == 0) SetWindowText(g_hWnd, kEnabledText);
+  
+  marvin::debug_log << "Marvin started successfully." << std::endl;
+}
+
+extern "C" __declspec(dllexport) void CleanupMarvin() {
+  DetourTransactionBegin();
+  DetourUpdateThread(GetCurrentThread());
+  DetourDetach(&(PVOID&)RealGetAsyncKeyState, OverrideGetAsyncKeyState);
+  DetourDetach(&(PVOID&)RealPeekMessageA, OverridePeekMessageA);
+  DetourTransactionCommit();
+
+  //SetWindowText(g_hWnd, "Continuum");
+
+  marvin::debug_log << "Shutting down Marvin." << std::endl;
+
+  g_Bot = nullptr;
+
+  marvin::debug_log.close();
+}
+
+BOOL WINAPI DllMain(HINSTANCE hInst, DWORD dwReason, LPVOID reserved) {
+  return TRUE;
+}
