68f9e4c24167b4ce42218b9f7aabdd66948d7b8f
jonpad01 <62527756+jonpad01@users.noreply.github.com>
2024-05-28 14:13:59 -0700

    Add option to accept compressed setship commands

--- Marvin/commands/SetShipCommand.h
+++ Marvin/commands/SetShipCommand.h
@@ -12,20 +12,32 @@ class SetShipCommand : public CommandExecutor {
     Blackboard& bb = bot.GetBlackboard();
     GameProxy& game = bot.GetGame();
 
-    std::vector<std::string> args = Tokenize(arg, ' ');
-
-    if (args.empty() || args[0].empty()) {
-      SendUsage(game, sender);
-      return;
-    }
-
     uint16_t number = 12;
 
-    if (std::isdigit(args[0][0])) {
-      number = atoi(args[0].c_str());
-    } else {
-      SendUsage(game, sender);
-      return;
+    // check if alias should have args
+    if (alias == "setship" || alias == "ss") {
+
+      std::vector<std::string> args = Tokenize(arg, ' ');
+
+      if (args.empty() || args[0].empty()) {
+        SendUsage(game, sender);
+        return;
+      }
+
+      if (std::isdigit(args[0][0])) {
+        number = atoi(args[0].c_str());
+      } else {
+        SendUsage(game, sender);
+        return;
+      }
+    } else {  // alias is a compressed alias (ss2)
+      std::string input = alias.substr(alias.size() - 1);
+      if (!input.empty() && std::isdigit(input[0])) {
+        number = atoi(input.c_str());
+      } else {
+        SendUsage(game, sender);
+        return;
+      }
     }
 
     if (number >= 1 && number <= 9) {
@@ -52,7 +64,20 @@ class SetShipCommand : public CommandExecutor {
   CommandAccessFlags GetAccess() { return CommandAccess_All; }
   void SetAccess(CommandAccessFlags flags) { return; }
   CommandFlags GetFlags() { return CommandFlag_Lockable; }
-  std::vector<std::string> GetAliases() { return {"setship", "ss"}; }
+  std::vector<std::string> GetAliases() { 
+
+    std::vector<std::string> alias_list{"setship", "ss"};
+    std::string alias_prefix = "ss";
+
+    // add alias for ss1 - ss9
+    for (std::size_t i = 1; i <= 9; i++) {
+      std::string alias = alias_prefix + std::to_string(i);
+      alias_list.push_back(alias);
+    }
+
+    return alias_list;
+   //return {"setship", "ss"}; 
+  }
   std::string GetDescription() { return "Sets the ship (9 = spec)"; }
   int GetSecurityLevel() { return 0; }
   CommandType GetCommandType() { return CommandType::Action; }
